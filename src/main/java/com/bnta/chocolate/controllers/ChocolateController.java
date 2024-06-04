6be015ceb6fbbccd4f89313b9b05a986eaac9fa3
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-06-04 17:49:53 +0100

    added getById routes

--- src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -6,9 +6,7 @@ import com.bnta.chocolate.services.ChocolateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,12 @@ public class ChocolateController {
     public ResponseEntity<List<Chocolate>> getAllChocolates() {
         return new ResponseEntity<>(chocolateService.getAllChocolates(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Chocolate> getChocolateById(@PathVariable long id) {
+        return new ResponseEntity<>(chocolateService.getChocolateById(id).get(), HttpStatus.OK);
+    }
+
 
 
 }
--- src/main/java/com/bnta/chocolate/controllers/EstateController.java
+++ src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -1,14 +1,13 @@
 package com.bnta.chocolate.controllers;
 
 
+import com.bnta.chocolate.models.Chocolate;
 import com.bnta.chocolate.models.Estate;
 import com.bnta.chocolate.services.EstateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,11 @@ public class EstateController {
     @GetMapping
     public ResponseEntity<List<Estate>> getAllEstates() {
         return new ResponseEntity<>(estateService.getAllEstates(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Estate> getEstateById(@PathVariable long id) {
+        return new ResponseEntity<>(estateService.getEstateById(id).get(), HttpStatus.OK);
     }
 
 }
